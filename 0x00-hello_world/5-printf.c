641bf006922e83b40621d856aa815e5ab9b341bb
timkanda <tmthkanda@gmail.com>
2022-09-12 01:15:13 -0700

    printing a message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: 'with proper grammar, but the outcome is a piece of art,'
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
