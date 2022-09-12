ff995da7d14c354c52300a44452ffad34e4e96a5
timkanda <tmthkanda@gmail.com>
2022-09-12 02:20:03 -0700

    printing with f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
  * Main - Entry point
  *
  * Return: Always 0
- * */
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
