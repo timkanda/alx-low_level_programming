3358a51ac48e13c96cab804304070074ef1a119c
timkanda <tmthkanda@gmail.com>
2022-09-12 03:14:01 -0700

    printing the size data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf("Size of char: %d bytes\n", sizeof(char *));
-	printf("\nSize of int:%d bytes\n", sizeof(int *));
-	printf("\nSize of long int: %d bytes\n", sizeof(long int *));
-	printf("\nSize of long long int: %d bytes\n", sizeof(long long int *));
-	printf("\nSize of float: %d bytes\n", sizeof(float *));
+	printf("Size of char: %d byte(s)\n", sizeof(char *));
+	printf("\nSize of int:%d byte(s)\n", sizeof(int *));
+	printf("\nSize of long int: %d byte(s)\n", sizeof(long int *));
+	printf("\nSize of long long int: %d byte(s)\n", sizeof(long long int *));
+	printf("\nSize of float: %d byte(s)\n", sizeof(float *));
 
 	return (0);
 }
