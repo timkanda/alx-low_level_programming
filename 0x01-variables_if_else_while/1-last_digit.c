a19a42f3c4b60039ddb3fd1073108e6587fdde5d
timkanda <tmthkanda@gmail.com>
2022-09-13 07:27:15 -0700

    assigning a random number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	scanf("%d", &n);
+	if (n > 5)
+		printf("%d and is greater than 5", n);
+	else if (n == 0)
+		printf("%d and is 0", n);
+	else if (n < 6)
+		printf("%d and is less than 6 and not 0", n);
+	return (0);
+
+}
