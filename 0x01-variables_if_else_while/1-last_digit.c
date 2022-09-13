8a30ccf473f65c22c757828c845977126da31017
timkanda <tmthkanda@gmail.com>
2022-09-13 10:54:34 -0700

    assigning randon numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,10 +16,10 @@ int main(void)
 	scanf("%d", &n);
 	LastDigit = n % 10;
 	if (n > 5)
-		printf("Last digit of %d is %d and is 0\n", n, LastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
 	else if (n == 0)
 		printf("Last digit of %d is %d and is 0\n", n, LastDigit);
-	else if (n < 6)
+	else if (n < 6 || n != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, LastDigit);
 	return (0);
 
