f3995e5eafca410145b1af26b231ff652757e8a3
timkanda <tmthkanda@gmail.com>
2022-09-14 05:46:13 -0700

    printing hexadecimals

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,13 +11,10 @@ int main(void)
 	char letters;
 
 	for (i = 0; i < 10; i++)
-	{
 		putchar(i);
-			for (letters = 'a'; letters <= 'f'; letters++)
-			{
-				putchar(letters);
-			}
-	}
+	for (letters = 'a'; letters <= 'f'; letters++)
+		putchar(letters);
+
 	putchar('\n');
 	return (0);
 
