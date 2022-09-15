55123b6d07fd3201eb246e8d8921fe55874fae0f
timkanda <tmthkanda@gmail.com>
2022-09-15 12:29:02 -0700

    printing alphabets in nested project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints alphabets in lowercase
+*
+*Return: void
+*/
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (aphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++
+	}
+	_putchar('\n');
+	return (0);
+}
