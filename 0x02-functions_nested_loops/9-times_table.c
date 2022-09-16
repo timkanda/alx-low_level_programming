3faa36f4e262ed111276b9979fc10d6bec141a8f
timkanda <tmthkanda@gmail.com>
2022-09-15 17:34:48 -0700

    printing the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 {
 	int ro, col, a;
 
-	for (ro = 0; ro < 9; ro++)
+	for (ro = 0; ro < 10; ro++)
 	{
 		_putchar('0');
 		_putchar(',');
@@ -27,7 +27,7 @@ void times_table(void)
 			}
 			_putchar((a % 10) + '0');
 
-			if (col <= 9)
+			if (col < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
