f35f7c69c542a65165af8052a8f8dfd779b65901
timkanda <tmthkanda@gmail.com>
2022-09-19 12:26:13 -0700

    resetting a value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,25 +8,5 @@
 */
 void reset_to_98(int *n)
 {
-	_putchar("%p\n", n);
 	*n = 98;
-}
-
-/**
-*main - changes the value of the variable
-*it is declared in, using a pointer
-*
-*Return: Always 0
-*/
-int main(void)
-{
-	int n;
-	int *p;
-
-	p = &n;
-	n = 50;
-	_putchar("%d\n", n);
-	reset_to_98(p);
-	_putchar("%d\n", n);
-	return (0);
 }
