b51aeeba66df1ec37bee3d679c350bf0910f6ff5
timkanda <tmthkanda@gmail.com>
2022-09-19 15:04:09 -0700

    printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*print_rev - prints a string in reverse
+*@s: the string to be printed
+*Return: Returns the reversed string
+*/
+void print_rev(char *s)
+{
+	*s = "";
+
+	print_rev(s);
+}
