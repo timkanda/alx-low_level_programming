60010a87c46341af3aac6b06763d9ea06c60183c
timkanda <tmthkanda@gmail.com>
2022-09-19 14:04:26 -0700

    printing a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_puts - prints a string
+*@str: the string to print
+*Return: strin is printed to stdout
+*/
+void _puts(char *str)
+{
+	str = "I love using betty to check my code";
+	puts(str);
+}
