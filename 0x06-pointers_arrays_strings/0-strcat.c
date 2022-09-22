cafaeac54d6c63c28783336e2c2dcb6e8c377518
timkanda <tmthkanda@gmail.com>
2022-09-22 04:34:26 -0700

    concantinating two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_strcat - concatenates two strings
+*@dest: input character
+*@src: input character
+*Return: returns dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int k = strlen(src);
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i + k] = src[i];
+	}
+	dest[i + k] = '\n';
+	return (dest);
+}
