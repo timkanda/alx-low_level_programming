bef8257cece6c40a832b492984b89c15c5f744e2
timkanda <tmthkanda@gmail.com>
2022-09-22 06:17:13 -0700

    concantinating strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,5 +9,16 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int b = 0;
+	int c = 0;
+
+	while (dest[b] != '\0')
+		b++;
+	while (src[c] != '\0' && c < n)
+	{
+		dest[b] = src[c];
+		b++;
+		c++;
+	}
+	return (dest);
 }
