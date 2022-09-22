c07ad9ce245e3cc9bb1db22e882569de56093b2f
timkanda <tmthkanda@gmail.com>
2022-09-22 08:41:25 -0700

    capitalizing all words

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,6 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *k);
-char *cap_string(char *);
-char *leet(char *);
+char *cap_string(char *a);
+char *leet(char *m);
 #endif
