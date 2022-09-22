1027bfb9e474765882080b2fe065d159c145b741
timkanda <tmthkanda@gmail.com>
2022-09-22 08:26:29 -0700

    capitalizing all words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,35 @@
 #include "main.h"
 
 /**
-*
-*
+*cap_string - capilizes all words of a string
+*@a: character input
+*Return: Always 0
 */
-char *cap_string(char *)
+char *cap_string(char *a)
 {
-	
+	int i, y;
+
+	int cap = 32;
+	int separators[] = {',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}', ' ', '\n', '\t', };
+	for (i = 0; a[i] != '\n'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] = a[i] - cap;
+		}
+
+		cap = 0;
+
+		for (y = 0; y <= 15; y++)
+		{
+			if (a[i] == separators[y])
+			{
+				y = 15;
+				cap = 32;
+			}
+		}
+	}
+
+	return (a);
 }
