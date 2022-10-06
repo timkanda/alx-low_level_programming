221ddcb62995877fe68d4b6280a8fa7ebc367ca3
timkanda <tmthkanda@gmail.com>
2022-10-06 05:02:18 -0700

    printing prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,3 +13,25 @@ int is_prime_number(int n)
 		return (1);
 	return (0);
 }
+
+/**
+*check_prime - checks if prime number
+*@n: input
+*@y: input
+*Return: check_prime
+*/
+int check_prime(int n, int y)
+{
+	if (y == 1)
+		return (1);
+	if (n % y == 0)
+		return (0);
+	else
+		return (check_prime(n, y - 1));
+}
+
+
+
+
+
+
