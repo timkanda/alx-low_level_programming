521b8ea60ca5be48c0f79938bef70472d387a164
timkanda <tmthkanda@gmail.com>
2022-11-03 07:30:26 -0700

    performing simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,86 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*op_add - prints sum
+*@a: input
+*@b: input
+*Return: 0
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*op_sub - prints the difference
+*@a: input
+*@b: input
+*Return: 0
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+*op_mul - prints the product
+*@a: input
+*@b: input
+*Return: 0
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+*op_div - prints the division
+*@a: input
+*@b: input
+*Return: 0
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+*op_mod - prints the reminder
+*@a: input
+*@b: input
+*Return: 0
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
