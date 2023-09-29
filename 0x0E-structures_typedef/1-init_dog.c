97cb88292d4f6fa5666d0aedf663cdba260bbc78
Timothy Kanda <98087672+timkanda@users.noreply.github.com>
2023-09-30 08:42:54 +1000

    commit file of typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,22 @@
+#include "dog.h"
 
+/**
+*init_dog - initialize a variable of type
+*@d: struct
+*@name: input name
+*@age: input age
+*@owner: input owner
+*return: 0
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+		if (d == NULL)
+			return;
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
