4279af1f4c012cc8e4b4482768d09f7035e87325
Timothy Kanda <98087672+timkanda@users.noreply.github.com>
2023-09-30 08:59:13 +1000

    commit file with if, else, while condition

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	scanf("%d", &n);
+
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+
+	return (0);
+
+
+}
