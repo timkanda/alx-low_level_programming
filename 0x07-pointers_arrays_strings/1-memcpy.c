3a150b2344851f2bb0c9773814f1863d8f7953cd
Timothy Kanda <98087672+timkanda@users.noreply.github.com>
2023-09-30 11:25:07 +1000

    commit more pointers files

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_memcpy - copies memory area
+*@dest: input character
+*@src: input character
+*@n: input
+*Return: Always 0
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for  (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
