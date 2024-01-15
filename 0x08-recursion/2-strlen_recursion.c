af28431834b576ef1af073a39e6c82a4844d2343
timkanda <tmthkanda@gmail.com>
2024-01-15 03:50:51 -0800

    printing length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,10 +5,10 @@
 *@s: input
 *Return: Always 0
 */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return(0);
+		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
